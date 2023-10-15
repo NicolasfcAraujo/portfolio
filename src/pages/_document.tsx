9a0ec8e0409bafe617705c757adee6150458aa6a
NICOLAS ARAUJO <nicolasfcaraujo@gmail.com>
2023-10-15 15:00:46 -0300

    apost added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <head>
-        <title>Nicolas Araujo's portfolio</title>
+        <title>Nicolas Araujo&apos;s portfolio</title>
         <link rel="shortcut icon" href="box.svg" type="image/x-icon" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
