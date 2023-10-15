6403335cdd9196fa8cc63efd072c30e7aa4f1584
NICOLAS ARAUJO <nicolasfcaraujo@gmail.com>
2023-10-15 14:33:22 -0300

    title and favicon changed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      <head>
+        <title>Nicolas Araujo's portfolio</title>
+        <link rel="shortcut icon" href="box.svg" type="image/x-icon" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      </head>
       <body>
         <Main />
         <NextScript />
